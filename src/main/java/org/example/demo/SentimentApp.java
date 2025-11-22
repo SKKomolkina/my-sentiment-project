f43460a2483eb63ffd2491a6598b5e873d8b657d
Skkomolkina <skomolkina@gmail.com>
2025-11-22 15:27:39 +0300

    Initial commit

---
+++ src/main/java/org/example/demo/SentimentApp.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class SentimentApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentimentApp.class, args);
+    }
+
+    @GetMapping("/api/sentiment")
+    public Map<String, String> analyze(@RequestParam(defaultValue = "") String text) {
+        // Простейший Mock-анализ
+        String sentiment = text.toLowerCase().contains("bad") ? "negative" : "positive";
+        return Map.of("sentiment", sentiment, "input", text);
+    }
+}
